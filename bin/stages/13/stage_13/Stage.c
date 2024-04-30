ac70737ea603e66c4439f92fe286e2b0254ca97e
Boonk3312 <theboonk7@hotmail.com>
2024-04-29 20:44:07 -0700

    Create Stage.c

---
+++ bin/stages/13/stage_13/Stage.c
@@ -0,0 +1,10 @@
+class Stage_13() {
+     /*
+      *   Code for Stage 13 (This level is custom !!!)
+      */
+
+     new BallRestorePowerup();
+     new Crystal(SpawnCount(10), CrystalBallCount(15));
+     new Crystal(SpawnCount(10), CrystalBallCount(15));
+     new Entity(FloatingCrystalShooter);
+}
